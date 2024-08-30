399c5a35e650983543d5014b6f371cd4d79dcb4a
ajsuper <60116786+ajsuper@users.noreply.github.com>
2024-08-29 19:17:35 -0500

    Uploaded files

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "testlib/mylibrary.h"
+#include </usr/include/c++/13/iostream>
+using namespace std;
+
+int main(){
+    sayHello();
+
+    int result = addNumbers(5, 7);
+    cout << "The result is: " << result << "\n";
+
+    return 0;
+}
---
+++ testlib/mylibrary.cpp
@@ -0,0 +1,13 @@
+#include "mylibrary.h"
+#include </usr/include/c++/13/iostream>
+using namespace std;
+
+void sayHello()
+{
+    cout << "HelloWorld!\n";
+}
+
+int addNumbers(int a, int b)
+{
+    return (a + b);
+}
